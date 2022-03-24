d71a8e6b80c0d93542eb2df3936225792f4c8e2a
Abhishek Tirkey <abhishek.tirkey@verifiably.com>
2022-03-25 00:52:36 +0530

    New and Create added

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progressValue: number = 0;
+  timer: any;
+  timerStopped?: boolean;
 
-  constructor() { }
+  constructor() { 
+    this.startTimer();
+  }
 
   ngOnInit(): void {
+  }
+
+  startTimer(): void {
+    this.timerStopped = false;
+    this.timer = setInterval(() => {
+      this.progressValue += 25;
+      if(this.progressValue === 100) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+
+  pauseTimer(): void {
+    clearInterval(this.timer);
+    this.timerStopped = true;
   }
 
 }
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Workouts {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-
+  workouts: Workouts[] = [
+    { value: 'crunches', viewValue: 'Crunches' },
+    { value: 'pull-ups', viewValue: 'Pull Ups' },
+    { value: 'lunges', viewValue: 'Lunges' },
+    { value: 'squats', viewValue: 'Squats' },
+  ];
   constructor() { }
 
   ngOnInit(): void {
